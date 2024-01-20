b81df9e156d7e55c7aca6d2724c8e096b8d2f902
Shiqi Mei <shiqi.mei@askcodebase.com>
2024-01-20 21:24:58 +0800

    Fetch data from wolai

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,73 @@
 import IconSearch from './assets/search.svg'
 import IconHome from './assets/home.svg'
+import { createEffect, createSignal } from 'solid-js'
+
+interface IMemo {
+  type: 'text' | 'image' | 'video'
+  content: string
+  created_at: number
+}
+
+function createLocalSignal(key: string, initialValue: IMemo[]) {
+  // Attempt to get the stored value from localStorage
+  const storedValue = localStorage.getItem(key);
+  // If there's a stored value, parse it, otherwise use the provided initial value
+  const [value, setValue] = createSignal(storedValue ? JSON.parse(storedValue) : initialValue);
+
+  // Function to update localStorage
+  const updateLocalStorage = () => {
+    localStorage.setItem(key, JSON.stringify(value()));
+  };
+
+  // Update localStorage whenever the value changes
+  createEffect(() => {
+    updateLocalStorage();
+  });
+
+  // Return the signal and the function to update it
+  return [value as unknown as () => IMemo[], setValue as (memos: IMemo[]) => void] as const;
+}
 
 function App() {
+  const [memos, setMemos] = createLocalSignal('memos', [])
+  createEffect(async () => {
+    const resp = await fetch('/api/memo/list')
+    const json = await resp.json() as IMemo[]
+    setMemos(json)
+  })
+
+  function renderMemo(memo: IMemo) {
+    switch (memo.type) {
+      case 'text': {
+        return (
+          <div class='message-content w-full max-w-[672px] bg-white rounded-md border border-message-border text-gray-700 p-4 text-sm whitespace-pre-wrap'>
+            {memo.content}
+          </div>
+        )
+      }
+      case 'image': {
+        return (
+          <img class='message-content min-w-[100px] min-h-[100px] max-w-[300px] bg-white rounded-md border border-message-border text-gray-700 text-sm' src={`/api/assets/${memo.content}`} alt={memo.content} />
+        )
+      }
+      case 'video': {
+        return (
+          <video controls width="250" class='message-content max-w-[300px] bg-white rounded-md border border-message-border text-gray-700 text-sm'>
+            <source src={`/api/assets/${memo.content}`} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        )
+      }
+    }
+  }
+
   return (
     <>
       <div class='sidebar w-[300px] h-screen bg-sidebar-background border-r border-seperator-color'>
         <div class='search-container p-4 cursor-pointer'>
           <div class='search w-full h-[32px] border border-input-border rounded-md text-input-foreground bg-input-background hover:bg-input-hover-background hover:text-input-hover-foreground flex items-center box-border p-2'>
             <IconSearch class='icon-search text-input-icon h-[16px] w-[16px] mr-2' />
-            <div class='search-placeholder flex-1 text-sm flex items-center justify-between'>
+            <div class='search-placeholder flex-1 text-sm flex items-center justify-between select-none'>
               Find Anything
               <span class='shortcut text-xs bg-shortcut-background border border-shortcut-border rounded ml-1 px-1'>
                 ⌘ K
@@ -31,22 +90,17 @@ function App() {
         </div>
       </div>
       <div class='main w-full h-screen flex flex-col'>
-        <div class='messages-container flex-1 h-full flex flex-col-reverse gap-4 p-4'>
-          <div class='message w-full flex'>
-            <img class='messaeg-author h-6 w-6 mr-3 rounded' src='avatar.jpeg' alt='avatar' />
-            <div class='message-content w-full max-w-[672px] h-[100px] bg-white rounded-md border border-message-border text-gray-700 p-2'>
-              This is a test message
-            </div>
-          </div>
-          <div class='message w-full flex'>
-            <img class='messaeg-author h-6 w-6 mr-3 rounded' src='avatar.jpeg' alt='avatar' />
-            <div class='message-content w-full max-w-[672px] h-[100px] bg-white rounded-md border border-message-border text-gray-700 p-2'>
-              This is a test message
-            </div>
-          </div>
+        <div class='messages flex-1 h-full flex flex-col-reverse gap-4 p-4 overflow-auto'>
+          {memos().map(memo => {
+            return (
+              <div class='message w-full flex'>
+                <img class='messaeg-author h-6 w-6 mr-3 rounded' src='avatar.jpeg' alt='avatar' />
+                {renderMemo(memo)}
+              </div>
+            )
+          })}
         </div>
-        <div id='editor-container' class='input w-full h-[350px] bg-white outline-none p-4 text-md text-[#262626]' contentEditable>
-        </div>
+        <div id='editor-container' class='input w-full h-[350px] bg-white outline-none p-4 text-md text-[#262626] border-t border-seperator-color' contentEditable />
       </div>
     </>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import solid from 'vite-plugin-solid'
 import svg from 'vite-plugin-solid-svg'
 
 export default defineConfig({
-  plugins: [solid(), svg()]
+  plugins: [solid(), svg()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://memolm-api.askcodebase.workers.dev/',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
